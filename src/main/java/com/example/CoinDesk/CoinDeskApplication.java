eabd82ceceb2b7c65674eed6c6952aeab3cfaed2
wey <weybrian@gmail.com>
2024-12-12 21:38:00 +0800

    init

---
+++ src/main/java/com/example/CoinDesk/CoinDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoinDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoinDesk/CoinDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoinDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
